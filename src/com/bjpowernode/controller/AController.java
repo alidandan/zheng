58e67ffbedb65d58b58cf3af6a41b5a92ad1969e
dell <1162676689@qq.com>
2020-12-16 17:08:56 +0800

    第一次提交

---
+++ src/com/bjpowernode/controller/AController.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: ssm
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: 郑进滔
+ * @CreateDate: 2020/12/16 17:03
+ * @Version: 1.0
+ * Copyright: Copyright (c) 2020
+ */
+public class AController {
+}
